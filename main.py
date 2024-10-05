63296177e5529ca8d82c4ce41879d09bab13cc90
MuhammadbinSajid <mohammedsajidamazai@gmail.com>
2024-10-05 17:06:07 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Take user input
+a = input("Enter a word: ")
+#program to checck break keyword
+for i in a: #iterate for loop
+    if (i == 'A'): #condition 1
+    #display result
+        print("A is found")
+        break #Break statement
+    else:
+        #display result
+        print("A is not found")
